4e748a48fdf544a469bcf887edc75194607d5ed3
Steven Borrelli <steve@borrelli.org>
2023-09-21 07:47:45 +0200

    delete existing manifests in generate
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- input/generate.go
+++ input/generate.go
@@ -4,6 +4,8 @@
 // NOTE(negz): See the below link for details on what is happening here.
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 
+// Remove existing and generate new input manifests
+//go:generate rm -rf ../package/input/
 //go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen paths=./v1beta1 object crd:crdVersions=v1 output:artifacts:config=../package/input
 
 package input
